ed4495968dfdbf1fa4be47350551a696757f4a64
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-06-23 20:40:12 +0200

    new version, spliting JS & CSS in separated files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <head>
+    <title>Wikimedia projects edits counter</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="wmcounter.css" media="screen" />
+    </head>
+<body>
+    <div id="wrapper">
+    <div id="content">
+
+    <p id="followus">
+    <!--
+    Join us on <a href="http://www.facebook.com/group.php?gid=287466429242">Facebook</a> and follow us on <a href="http://twitter.com/wmcounter">Twitter</a>!<br/>
+    Chat with us at <a href="http://webchat.freenode.net/?randomnick=1&channels=wikimediacounter">Freenode</a><br/>-->
+    1 Billionth edit <a href="onebillion.png">screenshot</a>!<br/>
+    </p>
+
+    <p id="translateit"><a href="http://en.wikipedia.org/wiki/User:Emijrp/Wikimedia_projects_edits_counter">Translate it</a>!</p>
+
+    <span id="header">Total edits in <a href="http://www.wikimedia.org">Wikimedia projects</a>:</span><br/>
+
+    <span id="counter">&nbsp;</span><br/>
+
+    <span id="footer"><a href="http://en.wikipedia.org/wiki/User:Emijrp/All_human_knowledge">All human knowledge</a> &ndash; <a href="http://en.wikipedia.org/wiki/Wikipedia:There_is_a_deadline">There is a deadline</a></span><br/>
+
+    <p id="donate"><a href="http://wikimediafoundation.org/wiki/Support_Wikipedia">Donate to Wikimedia Foundation</a></p>
+    <p id="f11">Press F11 for fullscreen</p>
+    <p id="author">Developed by <a href="http://wikitech.wikimedia.org/wiki/User:Emijrp">emijrp</a> (Inspired by <a href="http://www.7is7.com/software/firefox/partycounter.html">7is7</a>)</p>
+
+    </div>
+    </div>
+
+    <script type="text/javascript" src="wmcounter.data.js"></script>
+    <script type="text/javascript" src="wmcounter.js"></script>
+    
+</body>
+</html>
---
+++ wmcounter.data.js
@@ -0,0 +1,3 @@
+var editinit = 0;
+var timeinit = 0;
+var editrate = 0; //edits per milisecond
