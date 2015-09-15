9b6808e10c13b7578343580453831cc51c860b1e
emijrp <emijrp@gmail.com>
2015-09-15 20:11:15 +0200

    https

--- public_html/index.php
+++ public_html/index.php
@@ -13,25 +13,25 @@
     Join us on <a href="http://www.facebook.com/group.php?gid=287466429242">Facebook</a> and <br/>
     Chat with us at <a href="http://webchat.freenode.net/?randomnick=1&channels=wikimediacounter">Freenode</a><br/>-->
     1 Billionth edit <a href="onebillion.png">screenshot</a>!<br/>
-    In <a href="https://commons.wikimedia.org/wiki/Category:Wikimedia_projects_edits_counter">other conferences</a>
+    In <a href="//commons.wikimedia.org/wiki/Category:Wikimedia_projects_edits_counter">wiki conferences</a>
     </p>
 
     <p id="upper-right">
-    <a href="http://twitter.com/wmcounter">Follow us</a> on Twitter!<br/>
-    <a href="http://en.wikipedia.org/wiki/User:Emijrp/Wikimedia_projects_edits_counter">Translate it</a>!
+    <a href="//twitter.com/wmcounter">Follow us</a> on Twitter!<br/>
+    <a href="//en.wikipedia.org/wiki/User:Emijrp/Wikimedia_projects_edits_counter">Translate it</a>!
     </p>
 
-    <span id="header">Total edits in <a href="http://www.wikimedia.org">Wikimedia projects</a>:</span><br/>
+    <span id="header">Total edits in <a href="//www.wikimedia.org">Wikimedia projects</a>:</span><br/>
 
     <span id="counter">&nbsp;</span><br/>
 
-    <span id="footer"><a href="http://en.wikipedia.org/wiki/User:Emijrp/All_human_knowledge">All human knowledge</a> &ndash; 
-    <a href="http://tools.wmflabs.org/commons-coverage/">Commons Coverage</a> &ndash; 
-    <a href="http://en.wikipedia.org/wiki/Wikipedia:There_is_a_deadline">There is a deadline</a></span><br/>
+    <span id="footer"><a href="//en.wikipedia.org/wiki/User:Emijrp/All_human_knowledge">All human knowledge</a> &ndash; 
+    <a href="//tools.wmflabs.org/wlm-maps">Wiki Loves Monuments</a> &ndash; 
+    <a href="//en.wikipedia.org/wiki/Wikipedia:There_is_a_deadline">There is a deadline</a></span><br/>
 
-    <p id="donate"><a href="http://wikimediafoundation.org/wiki/Support_Wikipedia">Donate to Wikimedia Foundation</a></p>
+    <p id="donate"><a href="//wikimediafoundation.org/wiki/Support_Wikipedia">Donate to Wikimedia Foundation</a></p>
     <p id="f11">Press F11 for fullscreen</p>
-    <p id="author">Developed by <a href="https://en.wikipedia.org/wiki/User:Emijrp">emijrp</a> (Inspired by <a href="http://www.7is7.com/software/firefox/partycounter.html">7is7</a>)</p>
+    <p id="author">Developed by <a href="//en.wikipedia.org/wiki/User:Emijrp">emijrp</a> (Inspired by <a href="http://www.7is7.com/software/firefox/partycounter.html">7is7</a>)</p>
 
     </div>
     </div>
